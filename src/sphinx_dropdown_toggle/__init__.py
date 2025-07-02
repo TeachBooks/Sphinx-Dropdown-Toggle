48d39f9e413b852ab2178900d889dbc6f9976353
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2025-07-02 13:23:33 +0200

    Functionality implemented

--- src/sphinx_dropdown_toggle/__init__.py
+++ src/sphinx_dropdown_toggle/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from sphinx.application import Sphinx
 
 try:
@@ -5,11 +7,21 @@ try:
 except ImportError:
     __version__ = "1.0.0"
 
+def copy_javascript(app: Sphinx, exception):
+    # Copy the JavaScript file to the output directory
+    js_file = os.path.join(os.path.dirname(__file__), 'static', 'dropdown_toggle.js')
+    
+    with open(js_file,'r') as js:
+        js_content = js.read()
+    if app.builder.format == 'html' and not exc:
+        staticdir = os.path.join(app.builder.outdir, '_static')
+        outfile = os.path.join(staticdir,'dropdown_toggle.js')
+        with open(outfile,'w') as js:
+            js.write(js_content)
+
 def setup(app: Sphinx):
-    app.add_css_file('image_dark_mode.css')
-    app.add_config_value('inverter_saturation',1.5,'env')
-    app.add_config_value('inverter_all',True,'env')
-    app.connect('build-finished', copy_stylesheet)
+    app.add_js_file('dropdown_toggle.js')
+    app.connect('build-finished', copy_javascript)
     return {
         "version": __version__,
         "parallel_read_safe": True,
